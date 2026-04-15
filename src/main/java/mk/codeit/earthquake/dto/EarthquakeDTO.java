36badf5db1b4400bbcfbfe5eb883b048093b9a21
Mila Gjorgoska <milagjorgoska@Milas-MacBook-Air.local>
2026-04-15 20:18:25 +0200

    Add Earthquake model and DTO

--- src/main/java/mk/codeit/earthquake/dto/EarthquakeDTO.java
+++ src/main/java/mk/codeit/earthquake/dto/EarthquakeDTO.java
@@ -1,4 +1,12 @@
 package mk.codeit.earthquake.dto;
 
+import lombok.Data;
+
+@Data
 public class EarthquakeDTO {
+    private String title;
+    private String place;
+    private Double magnitude;
+    private String magType;
+    private Long time;
 }
--- src/main/java/mk/codeit/earthquake/model/Earthquake.java
+++ src/main/java/mk/codeit/earthquake/model/Earthquake.java
@@ -1,4 +1,20 @@
 package mk.codeit.earthquake.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "earthquakes")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
 public class Earthquake {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String place;
+    private Double magnitude;
+    private String magType;
+    private Long time;
 }
